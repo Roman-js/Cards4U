c666aa6f799d2f273a39fa61f58735db323ce0d8
Roman-js <liverkuzen1989@gmail.com>
2020-05-01 09:38:06 +0300

    Updates

---
+++ precache-manifest.1210d4ceeceae4d88004c6356a5176ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386630c2ef0ee06b71e8f3e36d57447b",
+    "url": "/template-typescript/index.html"
+  },
+  {
+    "revision": "d928af02ad75d4066925",
+    "url": "/template-typescript/static/css/main.1de9193d.chunk.css"
+  },
+  {
+    "revision": "2b97c8d257e20ee9b880",
+    "url": "/template-typescript/static/js/2.e6222c40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/template-typescript/static/js/2.e6222c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d928af02ad75d4066925",
+    "url": "/template-typescript/static/js/main.bfe1ff14.chunk.js"
+  },
+  {
+    "revision": "d68763a1155ba2ca8187",
+    "url": "/template-typescript/static/js/runtime-main.5d52bb24.js"
+  }
+]);
