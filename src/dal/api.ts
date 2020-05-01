7b869ebbd3e48bc37c056d1dabefe5352c446462
Roman-js <liverkuzen1989@gmail.com>
2020-05-01 19:48:10 +0300

    delete idea

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -7,26 +7,4 @@ const instance = axios.create({
 export const authApi = {
 
 
-    register(email: string, password: string){
-        return instance.post('auth/register', {email, password})
-            .then(res=>{return {res}})
-            .catch(fal=>fal)
-    },
-
-    forgotPass(email: string, html1: string, html2: string){
-        return instance.post('auth/forgot', {email, html1, html2})
-            .then(res=>{return {res}})
-            .catch(fal=>fal)
-    },
-
-    authMe(){
-        return instance.post('auth/me', {token: ''})
-            .then(res=>res)
-            .catch((fal)=>{return fal})
-    },
-
-    setNewPass(resetPasswordToken: string, password: string){
-        return instance.post('auth/set-new-password', {resetPasswordToken, password})
-    }
-
 }
