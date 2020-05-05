4181888886d6a28873e75c8805dd3c09a4e12098
PRoman93 <roman_prokopets@mail.ua>
2020-05-05 17:38:56 +0300

    do some moves in register part of project primarily

---
+++ src/bll/reducers/register-reducer.ts
@@ -0,0 +1,26 @@
+import {REGISTER_SUCCESS} from "../../ui/common/Constants";
+import {registerInitialState, registerSuccessActionType} from "../../ui/authorization/authTypes/registerTypes";
+
+
+const initialState:registerInitialState = {
+    email: '',
+    password: ''
+}
+
+const registerReducer = (state: registerInitialState = initialState, action: registerActionType):registerInitialState => {
+    switch (action.type) {
+        case REGISTER_SUCCESS: {
+            debugger
+            return {
+                ...state,
+                email: action.email,
+                password: action.password
+            }
+        }
+    }
+    return state
+}
+
+type registerActionType = registerSuccessActionType
+
+export default registerReducer
--- src/ui/common/Constants.ts
+++ src/ui/common/Constants.ts
@@ -1,4 +1,10 @@
+//constants for paths
+
 export const SIGN_IN = '/sign-in'
 export const REGISTER = '/register'
 export const FORGOT = '/forgot'
 export const SET_NEW_PASS = '/set-new-pass'
+
+//constants for reducers
+
+export const REGISTER_SUCCESS = 'auth/register/REGISTER-SUCCESS'
