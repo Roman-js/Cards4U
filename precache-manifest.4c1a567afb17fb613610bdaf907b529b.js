6ef769fdfdb3ddb35943df45af5ad43fb3e9be53
Roman-js <liverkuzen1989@gmail.com>
2020-06-10 09:55:56 +0300

    Updates

---
+++ precache-manifest.4c1a567afb17fb613610bdaf907b529b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14269b19e2ee5974dcb6b89ab31554c1",
+    "url": "/cards4u/index.html"
+  },
+  {
+    "revision": "39f780cf018a8cfb767d",
+    "url": "/cards4u/static/css/main.8416bae9.chunk.css"
+  },
+  {
+    "revision": "74d12a9039f804fc278b",
+    "url": "/cards4u/static/js/2.d2097e2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards4u/static/js/2.d2097e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f780cf018a8cfb767d",
+    "url": "/cards4u/static/js/main.12011ed5.chunk.js"
+  },
+  {
+    "revision": "6759c43efae0288e325a",
+    "url": "/cards4u/static/js/runtime-main.ffca8475.js"
+  },
+  {
+    "revision": "5b889088fbacafc2dd0853b3635ddf1a",
+    "url": "/cards4u/static/media/CardOfDeck1.5b889088.png"
+  },
+  {
+    "revision": "8c7d59c70080521cd2086ac813598a2f",
+    "url": "/cards4u/static/media/backImgOfProfile.8c7d59c7.jpg"
+  },
+  {
+    "revision": "c0e7873639aae929e88a54166726ef67",
+    "url": "/cards4u/static/media/cardOfQuestion.c0e78736.gif"
+  },
+  {
+    "revision": "9d5a7d919d1679273f1db34310116998",
+    "url": "/cards4u/static/media/cardOffAnswer.9d5a7d91.png"
+  },
+  {
+    "revision": "11c7932d58d122ccbf4b5e3a224876a7",
+    "url": "/cards4u/static/media/cardOnAnswer.11c7932d.png"
+  },
+  {
+    "revision": "6c12dc6e8f36346ccd8b637558a84c27",
+    "url": "/cards4u/static/media/headerUpdateField.6c12dc6e.jpg"
+  },
+  {
+    "revision": "6667e8693c5c00a32a7b585175affd8e",
+    "url": "/cards4u/static/media/updateBackgroundImg.6667e869.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/template-typescript/precache-manifest.8fecbb189fcc57fcc1885c0ad96c68b9.js"
+  "/cards4u/precache-manifest.4c1a567afb17fb613610bdaf907b529b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/template-typescript/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cards4u/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
